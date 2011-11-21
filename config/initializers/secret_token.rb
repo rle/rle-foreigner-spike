7eb862227febbf5eabf00ed948b5a66f10d50dd5
Rudi Engelbrecht <rudi@engelbrecht.me>
2011-11-21 12:59:57 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RleForeignerSpike::Application.config.secret_token = 'c2ed768b86b6071c75f9771a92f766f1ba18abd1f6e114188c5d2c6fc645c6766a5cc90b2ebfeca83d0d221a2063292901d51234e8eb9c1547bd4e82fae66b33'
